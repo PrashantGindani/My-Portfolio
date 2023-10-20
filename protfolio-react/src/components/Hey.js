26226067f6fe9320f4fdd9e0c3bb3d1be2f05a9a
Prashant Gindani <prashantgindani@gmail.com>
2023-10-21 03:23:33 +0530

    all changes

--- protfolio-react/src/components/Hey.js
+++ protfolio-react/src/components/Hey.js
@@ -6,7 +6,7 @@ return(
         <div style={{color:'white',display:'inline',fontSize:'10vw',textAlign:'left'}}>
             Hey,<br/>
             I'm <span style={{fontStyle:'italic',fontWeight:'bold'}}>Prashant</span></div>
-        <div style={{position:'relative'}}><img style={{position:'absolute',width:"45%",left:'7vw',bottom:'0'}} src='https://uploads-ssl.webflow.com/62b73781ed1f16c095e434e5/6328f63b2426b0f435af6810_my-image.png'></img></div>
+        <div style={{position:'relative'}}><img style={{position:'absolute',width:"45%",left:'7vw',bottom:'0'}} src='./My-Portfolio-pic_1.png'></img></div>
         <div style={{color:'white',fontStyle:'italic',textAlign:'right',fontSize:'20px',opacity:'0.5'}}>Digital Athlete,<br/> relentlessly training<br/> & improving skills.</div>
 
     </div>
